02c7ac3c7bf6ae292b77a89ea1495a54e2e730fa
dhruhil2612 <47570231+enji-coder@users.noreply.github.com>
2021-07-28 14:16:55 +0530

    scrapy sqlite

---
+++ quotes.toscrape.com/1.4.quote_web_scraping/quote_web_scraping/spiders/quotes_spider.py
@@ -0,0 +1,31 @@
+import scrapy
+from scrapy import item
+from ..items import QuoteWebScrapingItem
+
+class ExampleSpider(scrapy.Spider):
+    name = 'quotes'
+    allowed_domains = ['example.com']
+    start_urls = ['http://quotes.toscrape.com']
+
+    def parse(self, response):
+        items = QuoteWebScrapingItem()  # creating object of class 
+    
+        all_quotes = response.css('div.quote')  
+        # retrive all quotes title , author and tag details 
+        # note it retrive 1st page all data only 
+        for quotes in all_quotes:
+            desc = quotes.css('span.text::text').extract()
+            author = quotes.css('.author::text').extract()
+            tag = quotes.css('div.tags a::text').extract() 
+            
+            items['tag'] = tag 
+            items['desc'] = desc
+            items['author'] = author
+            
+            yield items  
+            # yield{
+            #     'desc': desc,
+            #     'author': author,
+            #     'tag': tag,
+            # }
+
---
+++ sample.py
@@ -0,0 +1,7 @@
+import math 
+
+print(math.pi)
+print(math.sqrt(64))
+print(math.sin(5))
+print(math.cos(6))
+print(math.factorial(5))
